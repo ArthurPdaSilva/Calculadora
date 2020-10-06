34e5132b7c4a2032b5bfea545c39d7024f94ad99
Arthur Pereira da Silva <arthurpdasillva@gmail.com>
2020-10-05 22:54:59 -0300

    Versão Final p3
    Agora vai

--- calc/dinamica.js
+++ calc/dinamica.js
@@ -1,41 +1,40 @@
 // Variáveis:
-let fundo = document.getElementById('fundo');
+let fundo = document.getElementById("fundo");
 let lista = [];
 
 // Chamar Função:
-document.addEventListener('keypress', (e) => {
-    if (e.which == 13) {
-        return resultado('=');
-    }
-})
+document.addEventListener("keypress", (e) => {
+  if (e.which == 13) {
+    return resultado("=");
+  }
+});
 
 // Limpar:
 function apagar() {
-    fundo.value = "";
+  fundo.value = "";
 }
 
 // Números:
 function calc(x = 0) {
-    lista = [];
-    fundo.value += x;
-
+  lista = [];
+  fundo.value += x;
 }
 
 // Operadores:
 function calcOpera(y = 0) {
-    if (lista.includes(y) || fundo.value.length == 0) {
-        return false;
-    } else {
-        fundo.value += y;
-        lista.push('-', '*', '+', '/');
-    }
+  if (lista.includes(y) || fundo.value.length == 0) {
+    return false;
+  } else {
+    fundo.value += y;
+    lista.push("-", "*", "+", "/", ",");
+  }
 }
 
 // Resultado:
 function resultado() {
-    let resultado = 0;
-    resultado = fundo.value;
-    fundo.value = "";
-    // Faz cálculos com strings;
-    fundo.value = eval(resultado);
+  let resultado = 0;
+  resultado = fundo.value;
+  fundo.value = "";
+  // Faz cálculos com strings;
+  fundo.value = eval(resultado);
 }
